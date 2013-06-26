307aae1a35597c602e94c35169c8409779bf6018
Forestviolence <forestviolence@gmail.com>
2013-06-26 15:01:42 +0100

    remote ip should now change according to the ip of the server

--- client/rec/js/conf.js
+++ client/rec/js/conf.js
@@ -1,4 +1,6 @@
-var remote_ip = "http://127.0.0.1";
+var remote_ip = location.host;
+
+//var remote_ip = "http://127.0.0.1";
 //var remote_ip = "http://129.241.10.122";
 
 // set to localhost as default untill i find some function to fetch server ipadress.
