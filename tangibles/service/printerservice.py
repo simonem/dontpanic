7e8618342ed196f5a992b83965b139f02ce0e495
Simone Mora <morasimone@gmail.com>
2013-06-27 12:30:19 +0200

    added printerservice class for the thermal printer

---
+++ tangibles/service/printerservice.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+from Adafruit_Thermal import *
+
+printer = Adafruit_Thermal("/dev/ttyAMA0", 19200, timeout=5)
+
+printer.justify('C')
+printer.setSize('L')
+printer.println("Don't Panic!")
