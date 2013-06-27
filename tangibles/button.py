473f142f155376131c667e397daba536215eb208
Forestviolence <forestviolence@gmail.com>
2013-06-27 10:41:34 +0100

    added button.py for buttonpressdetection

---
+++ tangibles/button.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+from socketIO_client import SocketIO
+try: import simplejson as json
+except: import json
+import time
+
+class RPiButton:
+
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(24, GPIO.IN)
+
+    def get_button_pressed(socket):
+        while True:
+            inputValue = GPIO.input(24)
+            if(inputValue == True):
+                print 'The button is pressed'
+                print 'telling server to end turn'
+                endturncommand = { 'type':'end_turn'} 
+                socket.emit('game_command', json.dumps(endturncommand))
+                time.sleep(.3)
+            time.sleep(.01)
+
+    
