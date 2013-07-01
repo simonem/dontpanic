b54e364c461090c4300bbee0726eb38785e467aa
Simone <morasimone@gmail.com>
2013-07-01 15:27:38 +0000

    final commit

--- tangibles/service/button.py
+++ tangibles/service/button.py
@@ -2,23 +2,24 @@ import RPi.GPIO as GPIO
 from time import sleep
 import os
 
-
 class RPiButton:
 
-    def __init__(self, next_turn, use_card):
+    def __init__(self, next_turn, use_card, remove_action):
         global next_turn_function
 	global use_card_function
+	global remove_action_function
+	global p
+	remove_action_function = remove_action
         next_turn_function = next_turn
 	use_card_function = use_card
 	global isNotReading
 	isNotReading = True
-	global p
 	p=os.popen('/usr/bin/zbarcam --raw --nodisplay -Sdisable -Scode39.enable /dev/video0','r')
 
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.add_event_detect(18, GPIO.FALLING, callback=self.get_button_pressed, bouncetime=5000)
+	GPIO.add_event_detect(18, GPIO.BOTH, callback=self.get_button_pressed, bouncetime=5000)
 	GPIO.add_event_detect(4, GPIO.FALLING, callback=self.start_read_barcode, bouncetime=5000)
 
     def get_button_pressed(self, channel):
@@ -29,18 +30,26 @@ class RPiButton:
     def start_read_barcode(self, channel):
 		global isNotReading
 		global use_card_function
+		global remove_action_function
 		global blist
+		global p
 		if(isNotReading):
 			isNotReading = False
+
 			print 'looking for barcode key was pressed'
 			code = p.readline()
+			#p.close()
 			if code:
 				print 'got barcode:', code
 				isNotReading = True
-				for i in range(len(blist)):
-					if(blist[i] == int(code)):
-						os.system('mpg321 success.mp3 &')
-						use_card_function(i)
+				if(code == 'BLOCK\n'):
+					os.system('mpg321 /home/pi/dontpanic/tangibles/service/success.mp3 &')
+					remove_action_function()
+				else:
+					for i in range(len(blist)):
+						if(blist[i] == int(code)):
+							os.system('mpg321 /home/pi/dontpanic/tangibles/service/success.mp3 &')
+							use_card_function(i)
     def updateList(self, list):
 	global blist
 	blist = list
--- tangibles/service/lcdservice.py
+++ tangibles/service/lcdservice.py
@@ -7,18 +7,35 @@ class LcdService:
 	
 	def __init__(self):
 		global lcd
+		global gamerunning
+		gamerunning = True
 		lcd = Adafruit_CharLCD()
 		lcd.begin(16,4)
 	
 	def updateLCD(self,player,turn,apleft,panic):
-		global lcd		
-		lcd.clear()
-		lcd.setCursor(0,0)
-		lcd.message("Player: "+str(player))
-		lcd.setCursor(0,1)
-		lcd.message("Turn: "+str(turn))
-		lcd.setCursor(0,2)
-		lcd.message("Actions left: "+str(apleft))
-		lcd.setCursor(0,3)
-		lcd.message("Panic in: "+str(panic))
+		global lcd
+		global gamerunning
+		if(gamerunning):
+			lcd.clear()
+			lcd.setCursor(0,0)
+			lcd.message("Player: "+str(player))
+			lcd.setCursor(0,1)
+			lcd.message("Turn: "+str(turn))
+			lcd.setCursor(0,2)
+			lcd.message("Actions left: "+str(apleft))
+			lcd.setCursor(0,3)
+			lcd.message("Panic in: "+str(panic))
 
+	def game_is_lost(self):
+		global lcd
+		global gamerunning
+		gamerunning = False
+		lcd.setCursor(0,0)
+		lcd.message("Game is Lost!")
+
+	def game_is_won(self):
+		global lcd
+		global gamerunning
+		gamerunning = False
+		lcd.setCursor(0,0)
+		lcd.message("Game is Won!")
